13c9633b6e46b1c4266a3c20aed2226f7d833f9f
ASIA <ASIA@DESKTOP-OUKCVRD>
2020-10-15 16:08:56 +0700

    Bai_5

---
+++ Example_05/src/main/java/com/nguyenvanhau/Reservation.java
@@ -0,0 +1,33 @@
+package com.nguyenvanhau;
+
+public class Reservation {
+	private String Gender;
+	private String firstName;
+	private String lastName;
+	public Reservation() {
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		 this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return Gender;
+	}
+	public void setGender(String gender) {
+		Gender = gender;
+	}
+}
+	
